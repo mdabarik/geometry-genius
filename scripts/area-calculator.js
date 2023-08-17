8bd4408b59723d663cbc6e56ab12df8b9058c8f3
mdabarik <mdabarik19@gmail.com>
2023-08-18 00:44:32 +0600

    validation added

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -2,6 +2,10 @@
 function calculateTriangleArea() {
     const base = getInputValue('triangle-base');
     const height = getInputValue('triangle-height');
+    if (isNaN(base) || isNaN(height)) {
+        alert("please insert number.");
+        return;
+    }
     const area = 0.5 * base * height;
     setTextFieldText('show-rect-area', area);
 }
@@ -28,9 +32,6 @@ function calculateEllipseArea() {
     setTextFieldText('show-ellipse-area', area.toFixed(2));
 }
 
-
-
-
 // reuseable get input value field in number
 function getInputValue(fieldId) {
     const inputField = document.getElementById(fieldId);
@@ -44,3 +45,13 @@ function setTextFieldText(elementId, area) {
     document.getElementById(elementId).innerText = area;
 }
 
+
+// Data validation
+/*
+(NaN = Not a number)
+1. set the proper type of the input field. (number, data, email)
+2. check type using typeof
+3. NaN means: Not a Number. isNaN is checking whether the input is not a number or not.
+
+// validate input
+*/
