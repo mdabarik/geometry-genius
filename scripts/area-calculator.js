e1525b52000c892755708fdf6fc24b14c086c582
mdabarik <mdabarik19@gmail.com>
2023-08-18 01:05:01 +0600

    final touch

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -8,28 +8,45 @@ function calculateTriangleArea() {
     }
     const area = 0.5 * base * height;
     setTextFieldText('show-rect-area', area);
+    addToCalculationEntry('Triangle', area);
 }
 
 function calculateRectangleArea() {
     const width = getInputValue('rectangle-width');
     const height = getInputValue('rectangle-height')
+    if (isNaN(width) || isNaN(height)) {
+        alert("please insert number.");
+        return;
+    }
     const area = width * height;
     setTextFieldText('show-rectangle-area', area);
+    addToCalculationEntry('Rectangle', area);
+
 }
 
 function calculateParallelogramArea() {
     const height = getInputValue('parallelogram-height');
     const width = getInputValue('parallelogram-width');
+    if (isNaN(height) || isNaN(width)) {
+        alert("please insert number.");
+        return;
+    }
     const area = height * width;
     setTextFieldText('show-parallelogram-area', area);
-
+    // add to calculation entry
+    addToCalculationEntry('Parallelogram', area);
 }
 
 function calculateEllipseArea() {
     const largeRadius = getInputValue('ellipse-major-radius');
     const smallRadius = getInputValue('ellipse-minor-radius');
+    if (isNaN(largeRadius) || isNaN(smallRadius)) {
+        alert("please insert number.");
+        return;
+    }
     const area = Math.PI * largeRadius * smallRadius;
     setTextFieldText('show-ellipse-area', area.toFixed(2));
+    addToCalculationEntry('Ellipse', area);
 }
 
 // reuseable get input value field in number
@@ -44,6 +61,22 @@ function getInputValue(fieldId) {
 function setTextFieldText(elementId, area) {
     document.getElementById(elementId).innerText = area;
 }
+
+
+// add to calculation entry
+function addToCalculationEntry(areaType, area) {
+    const calculationEntry = document.getElementById('calculation-entry');
+
+    const count = calculationEntry.childElementCount;
+
+    const p = document.createElement('p');
+    p.classList.add('my-2')
+
+    p.innerHTML = `${count} ${areaType} ${area} cm<sup>2</sup> <button class='btn btn-success'>Convert</button>`
+
+    calculationEntry.appendChild(p);
+}
+
 
 
 // Data validation
